3be0526402c0923af7889559358cbeec3fd5d01d
Raghu Betina <raghu@firstdraft.com>
2020-05-31 19:37:46 +0000

    Added a page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def dropdown
+    render({ :template => "pages/dropdown.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
-
+  get("/", { :controller => "application", :action => "dropdown" })
 
 end
